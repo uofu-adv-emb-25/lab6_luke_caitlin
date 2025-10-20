dd96883914bd6181662ee60fbd7f63372e56a1bd
Luke Stillings <44128419+lukestillings@users.noreply.github.com>
2025-10-20 14:07:27 -0600

    Setup test file

--- test/test.c
+++ test/test.c
@@ -1,35 +1,32 @@
 #include <stdio.h>
 #include <pico/stdlib.h>
-#include <stdint.h>
-#include <unity.h>
+#include <FreeRTOS.h>
+#include <task.h>
 #include "unity_config.h"
+#include <unity.h>
 
-void setUp(void) {}
-
-void tearDown(void) {}
-
-void test_variable_assignment()
+void setUp(void)
 {
-    int x = 1;
-    TEST_ASSERT_TRUE_MESSAGE(x == 1,"Variable assignment failed.");
 }
 
-void test_multiplication(void)
+void tearDown(void)
 {
-    int x = 30;
-    int y = 6;
-    int z = x / y;
-    TEST_ASSERT_TRUE_MESSAGE(z == 5, "Multiplication of two integers returned incorrect value.");
 }
 
-int main (void)
+void supervisor(void *params)
+{
+    while (1) {
+        sleep_ms(5000); // Give time for TTY to attach.
+        printf("Start tests\n");
+        UNITY_BEGIN();
+        UNITY_END();
+    }
+}
+
+int main(void)
 {
     stdio_init_all();
-    sleep_ms(5000); // Give time for TTY to attach.
-    printf("Start tests\n");
-    UNITY_BEGIN();
-    RUN_TEST(test_variable_assignment);
-    RUN_TEST(test_multiplication);
-    sleep_ms(5000);
-    return UNITY_END();
+
+    xTaskCreate(supervisor, "supervisor", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY+4, NULL);
+    vTaskStartScheduler();
 }
