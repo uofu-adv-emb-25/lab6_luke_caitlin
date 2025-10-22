1600dc735a40897586e2ac9d0cad2cbd021bbc7d
Luke Stillings <44128419+lukestillings@users.noreply.github.com>
2025-10-22 13:14:21 -0600

    Test priority inheritance

--- test/test.c
+++ test/test.c
@@ -59,6 +59,37 @@ void test_priority_inversion(void)
     vTaskDelete(task_3);
 }
 
+void test_priority_inheritance(void)
+{
+    TaskHandle_t task_1;
+    TaskHandle_t task_2;
+    TaskHandle_t task_3;
+
+    SemaphoreHandle_t semaphore = xSemaphoreCreateMutex();
+    xSemaphoreGive(semaphore);
+    xTaskCreate(take_lock, "task_1", configMINIMAL_STACK_SIZE, &semaphore, tskIDLE_PRIORITY+1, &task_1);
+    vTaskDelay(100);
+    xTaskCreate(busy_busy, "task_2", configMINIMAL_STACK_SIZE, &semaphore, tskIDLE_PRIORITY+2, &task_2);
+    xTaskCreate(take_lock, "task_3", configMINIMAL_STACK_SIZE, &semaphore, tskIDLE_PRIORITY+3, &task_3);
+
+    vTaskDelay(200);
+
+    TEST_ASSERT(eTaskGetState(task_1) == eSuspended);
+    TEST_ASSERT(eTaskGetState(task_2) == eReady);
+    TEST_ASSERT(eTaskGetState(task_3) == eBlocked);
+
+    vTaskResume(task_1);
+    vTaskDelay(200);
+
+    TEST_ASSERT(eTaskGetState(task_1) == eReady);
+    TEST_ASSERT(eTaskGetState(task_2) == eReady);
+    TEST_ASSERT(eTaskGetState(task_3) == eSuspended);
+
+    vTaskDelete(task_1);
+    vTaskDelete(task_2);
+    vTaskDelete(task_3);
+}
+
 void supervisor(void *params)
 {
     while (1) {
@@ -66,6 +97,7 @@ void supervisor(void *params)
         printf("Start tests\n");
         UNITY_BEGIN();
         RUN_TEST(test_priority_inversion);
+        RUN_TEST(test_priority_inheritance);
         UNITY_END();
     }
 }
